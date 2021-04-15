0eca2e37c6ea4679997785e45502aa1846a1a4c5
nency-shobhashana <user197252@telehouse-moboware.dmarc.si1.atlanticmetro.net>
2021-04-14 20:41:11 -0400

    Added Collection View

---
+++ Capstone_CodeBrewer_BuildJobProfile/ResumeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ResumeCollectionViewCell.swift
+//  Capstone_CodeBrewer_BuildJobProfile
+//
+//  Created by user197252 on 4/14/21.
+//
+
+import UIKit
+
+class ResumeCollectionViewCell: UICollectionViewCell {
+    
+}
