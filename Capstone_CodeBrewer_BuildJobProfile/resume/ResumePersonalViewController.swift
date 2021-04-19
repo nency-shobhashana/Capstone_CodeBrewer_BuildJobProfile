5c39197f0928464c81bc8d34bce041dc5ecdd316
nency-shobhashana <shobhashananency@gmail.com>
2021-04-18 23:57:10 -0400

    Restriction on number uifield

--- Capstone_CodeBrewer_BuildJobProfile/resume/ResumePersonalViewController.swift
+++ Capstone_CodeBrewer_BuildJobProfile/resume/ResumePersonalViewController.swift
@@ -11,10 +11,9 @@ import MaterialComponents.MaterialTextControls_OutlinedTextFields
 import MaterialComponents.MaterialTextControls_OutlinedTextAreasTheming
 import MaterialComponents.MaterialTextControls_OutlinedTextFieldsTheming
 
-class ResumePersonalViewController: UIViewController, MDCBottomSheetMethod {
+class ResumePersonalViewController: UIViewController, MDCBottomSheetMethod, UITextFieldDelegate {
     
     weak var resumeDetailVC: ResumeDetailViewController!
-    
     
     @IBOutlet weak var firstNameTF: MDCOutlinedTextField!
     @IBOutlet weak var lastNameTF: MDCOutlinedTextField!
@@ -26,12 +25,25 @@ class ResumePersonalViewController: UIViewController, MDCBottomSheetMethod {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        phoneTF.delegate = self
+        //MARK:- UITextField Delegate
+        
+       
+        
         // load data from core data to UI component
         firstNameTF.text = resumeDetailVC.resumeData["firstName"] as? String ?? ""
         lastNameTF.text = resumeDetailVC.resumeData["lastName"] as? String ?? ""
         emailTF.text = resumeDetailVC.resumeData["email"] as? String ?? ""
         phoneTF.text = resumeDetailVC.resumeData["phone"] as? String ?? ""
         addressTF.text = resumeDetailVC.resumeData["address"] as? String ?? ""
+        
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let allowedCharacters = "+1234567890"
+        let allowedCharcterSet = CharacterSet(charactersIn: allowedCharacters)
+        let typedCharcterSet = CharacterSet(charactersIn: string)
+        return allowedCharcterSet.isSuperset(of: typedCharcterSet)
     }
     
     func onDismiss() {
